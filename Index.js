755e60e4abb7efff00b4726d57fcb96860b5ea0f
NerfThisxD <nerfthishaha@outlook.com>
2023-12-01 20:43:31 -0300

    Fiz o desafio do Felipe da Dio

--- Index.js
+++ Index.js
@@ -1,1 +1,50 @@
-console.log("Ola Mundo")
+//Meu Primeiro Desafio da DIO - Prof Felipe
+//Textos foram alterados do desafio original
+//Classificação de Heroi
+
+let  pdl = 2545
+let nome = "SweeT"
+
+if (pdl<=1000) {
+
+    console.log("Tu é pessímo " +nome+" Teu elo é MADEIRA, merecia nem elo  com um total de "+pdl+" de PDL")
+
+}else if (pdl>=1001 && pdl <= 2000){
+
+ console.log("Tu é bem ruizinho " +nome+" Teu elo é FERRO com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 2001 && pdl <= 5000){
+
+    console.log("'brozinho aqui é rei' ja diria nosso querido amigo SOLOQPATRICKVRAU. " +nome+" Teu elo é BRONZE com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 5001 && pdl <= 6000){
+
+    console.log("Ta ficanod bonzinho hein " +nome+" Teu elo é FERRO com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 6001 && pdl <= 7000){
+
+    console.log("Ta pagando quem pra jogar pra voce? " +nome+" Teu elo é PRATA com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 7001 && pdl <= 8000){
+
+    console.log("Tu ficando quentinho hein " +nome+" Teu elo é OURO com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 8001 && pdl <= 9000){
+
+    console.log("Ta vendo o calor? É tu ficando quente no joguinho " +nome+" Teu elo é PLATINA com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 9001 && pdl <= 10000){
+
+    console.log("Infinito e alem" +nome+" Teu elo é ASCENDENTE com um total de "+pdl+" de PDL")
+
+}else if (pdl>= 10001 && pdl <= 10999){
+
+    console.log("Tu é foda amigão" +nome+" Teu elo é IMORTAL com um total de "+pdl+" de PDL")
+
+}else if (pdl>=11000){
+
+    console.log("NÃO TEM COMO, JOGA Y JOGA, É UMA MAQUINA DE GANHAR " +nome+" Teu elo é RADIANTE, O DONO DO JOGUINHO com um total de "+pdl+" de PDL")
+    
+}
+console.log("")
+console.log("Caso não tenha gostado da sua classificação? Melhore. Obrigado por participar.")
